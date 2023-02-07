98be9f03daff1e0497d668a277fb486ed51c3a0a
Basnayaka B.M.H.S <hiransanjeewaa@gmail.com>
2023-02-07 04:34:59 -0800

    Add files via upload

---
+++ backend/e-commerce_system/src/test/java/com/example/ecommerce_system/ECommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
