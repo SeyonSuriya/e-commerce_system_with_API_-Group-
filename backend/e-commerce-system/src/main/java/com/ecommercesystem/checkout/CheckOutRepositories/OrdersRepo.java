fa8231d2454d4a072ae3f16ab54118f1d9f5bcfb
Basnayaka B.M.H.S <hiransanjeewaa@gmail.com>
2023-03-04 01:09:44 -0800

    New quaries added for deleting orders

--- backend/e-commerce-system/src/main/java/com/ecommercesystem/checkout/CheckOutRepositories/OrdersRepo.java
+++ backend/e-commerce-system/src/main/java/com/ecommercesystem/checkout/CheckOutRepositories/OrdersRepo.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @EnableJpaRepositories
 @Repository
 public interface OrdersRepo extends JpaRepository<orders,Integer> {
@@ -16,7 +18,21 @@ public interface OrdersRepo extends JpaRepository<orders,Integer> {
     @Modifying
     @Query(value=" Insert into orders (address,item_id,item_price,item_units,order_status,orderid,userid) Values(?1,?2,?3,?4,'Placed',?5,?6)", nativeQuery=true)
     Integer purchaceItem(String address,Integer item_id,Integer Product_price,Integer Units,Integer orderid,Integer userid);
-
     @Query(value = "Select orderid from orders ORDER BY orderid DESC Limit 1")
     Integer getNextOrderId();
+
+    @Query(value="select * from orders a where a.userid=:userid", nativeQuery=true)
+    List<orders> getorders(Integer userid);
+
+    @Transactional
+    @Modifying
+    @Query(value="delete from orders where userid=?1 and orderid=?2", nativeQuery=true)
+    Integer canselOrder(Integer userid,Integer orderid);
+
+    @Transactional
+    @Modifying
+    @Query(value="delete from orders where userid=?1 and reference=?2", nativeQuery=true)
+    Integer canselItem(Integer userid,Integer reference);
+
+
 }
