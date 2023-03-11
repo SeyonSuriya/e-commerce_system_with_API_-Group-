f64b612bf10dc49ea4bb5c406a7bad479ee14dca
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-03-11 10:38:33 +0530

    modified:Footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import './footerstyle.css';
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='containerfooter'>
+       <div className='firstline'>
+       <h3><a>Tech World</a></h3>
+       <h5><a>Terms</a></h5>
+       <h5><a>License</a></h5>
+       <h5><a>Market</a></h5>
+       <h5><a>All Product</a></h5>
+       </div>
+       <div className='secondline'>
+       <h3><a>Help</a></h3>
+       <h5><a>Help Center</a></h5>
+       <h5><a>Authors</a></h5><a></a>
+       </div> 
+       <div className='thirdline'>
+        <h3><a>OUR COMMUNITY</a></h3>
+        <h5><a>Community</a></h5>
+        <h5><a>Blog</a></h5>
+        <h5><a>Shopping Cart</a></h5>
+        <h5><a>My Account</a></h5>
+       </div>
+       <div className='sendmassage'>
+       fourth
+       </div>  
     </div>
   )
 }
