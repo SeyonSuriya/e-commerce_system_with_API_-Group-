d3a32154b211f37b02dad908f50866f179de3c39
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-03-14 23:49:03 +0530

    Login Page now connected with Backend

--- frontend/src/components/loginform.js
+++ frontend/src/components/loginform.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 import "./loginform.css"
 
-export default function loginform() {
+
+export default function Loginform(props) {
+  const [email,setEmail]=useState(' ');
+  const [password,setPassword]=useState(' ');
+
+  function onCreate(e) {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+
+    const postData = {
+      email,
+      password,
+    };
+    axios.post(
+      'http://localhost:8080/ecommerce/login',
+      postData,
+      ).then(response=>{
+        console.log(response.data)
+        if (response.data === 'Login Granted') {
+           document.getElementById('errorMessage').innerHTML=response.data;
+        }else{
+          document.getElementById('errorMessage').innerHTML=response.data;
+        }
+      })
+  }
   return (
        <div className ="log">
          <h1> LOGIN</h1>
-         <form>
-            Email :  <input type ="email" placeholder="Email"/><br/><br/>
-            Password : <input type ="password" placeholder="password"/>   
+         <form onSubmit={onCreate}>
+
+      Enter your name : <input type='text' name='username' value={email} onChange={(e)=>setEmail(e.target.value)}></input><br />
+      Enter your Password : <input type='text' name='password' value={password} onChange={(e)=>setPassword(e.target.value)}></input><br />
+        
+      <span id='errorMessage' className='ErrorMessage'></span>
                 <div className="Log-btn">
                     <br/>
                     <button type='submit'>Log in</button>
                 </div>
-         </form>       
+          </form>       
                 <p className="text"> OR LOGIN USING</p>
                 <div className="extra-Log">
                     <div className="Facebook"></div>
@@ -21,18 +50,5 @@ export default function loginform() {
        </div>
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
