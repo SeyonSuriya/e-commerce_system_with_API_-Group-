b385bad07e86e93fee4de2130ff9991e4131398a
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-03-26 14:06:18 +0530

    Changed Email verification page need to ad css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import RegisterPage from './pages/RegisterPage';
 import EmailVerification from './pages/EmailVerification';
 import ResetPassword from './pages/ResetPassword';
 import Product from './pages/Product';
+import VerifyEmail from './pages/VerifyEmail';
 
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
         <Route path="/emailverification" element={<EmailVerification />}/>
         <Route path="/resetpassword" element={<ResetPassword />}/>
         <Route path="/product" element={<Product />}/>
+        <Route path="/verifyemail" element={<VerifyEmail />}/>
       </Routes>
     </BrowserRouter>
     </div>
