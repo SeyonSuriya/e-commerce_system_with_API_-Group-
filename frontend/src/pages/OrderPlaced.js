7d98d0805fd7839e8094fac0770510c803720583
Yadeeshani Shakilanka Edirisinghe <syadeeshani@gmail.com>
2023-04-04 02:53:32 +0530

    few changes were done for the orderplace page

--- frontend/src/pages/OrderPlaced.js
+++ frontend/src/pages/OrderPlaced.js
@@ -1,14 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import Orderplaceimg01 from '../assests/Order place img 01.png';
+import "../components/orderplaced.css";
 
 export default function OrderPlaced(){
 
     return(
         <div>
-        <section>
+          
+        <section className='orderplace-section'>
+            <div className='orderplace-content'>
+                <div className='order-image'>
+                    <img src={Orderplaceimg01} className='order_img'/>
+                </div>
+                <h1>Your Order Is Complete</h1>
+                <h4>Thank you for your purchase!</h4>
+                <p id='order_text01'>Your order ID is : <b>order id</b></p>
+                <p id='order_text02'>You will receive an confirmation email with details of yur order</p>
+            </div>
 
 
-            
         </section>
 
 
