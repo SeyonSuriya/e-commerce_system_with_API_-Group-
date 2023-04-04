cb1ef218778d2e769d39323a0cc63f19beec949a
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-04-05 02:53:13 +0530

    Forgot password working well Change password page need to connect with frontend

--- frontend/src/pages/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword.js
@@ -1,15 +1,59 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+
 import "../components/forgotpassword.css";
+import  { useState } from 'react'
 import Forgotpasswordimg02 from '../assests/Forgotpassword img 02.png';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
 
 export default function ForgotPassword(){
+   // const [cookies,setCookie] = useCookies(['user']);
+
+    const [email,setEmail]=useState(' ');
+
+
+
+  function ResetPasswordEmail(params) {
+    params.preventDefault();
+    axios.post(
+      'http://localhost:8080/ecommerce/resetpassword?email='+email,
+     
+      ).then(response=>{
+        console.log(response.data)
+        if (response.data==='Reset Password link sent') {
+          document.getElementById('Message').innerHTML="<br/>"+response.data+"<br/>";
+        }else{
+          document.getElementById('Message').innerHTML="<br/>"+response.data+"<br/>";
+        }
+      }
+       )
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return(
         <div>
-            <Header/>
+           
                 <section className="forgot-password-section">
                     <div className="forgot-container">
                         <div className='flex-conatiner01'>
@@ -18,17 +62,20 @@ export default function ForgotPassword(){
                             <img src={Forgotpasswordimg02} className='forgot_img'id='forgot_img' alt=''/>
                             </div><b/>
                             <p id="forgot-text"> Please enter
-                            your <b>Username</b> or <b>Email address.</b><br /> You will
+                            your <b>Email address.</b><br /> You will
                             receive an email message with instructions on how to reset
                             your password.</p>
                             <b/>
 
          
-                                <form >
+                                <form onSubmit={ResetPasswordEmail}>
                                 
-                                    <label className="forgot_form">Username/Email Address</label><br/>
-                                    <input type="email" placeholder="Username or Email " className='forgot-form01'></input>
-                                    <br />                           
+                                    <label className="forgot_form">Email Address</label><br/>
+                                    <input type="email" placeholder="Email " className='forgot-form01' value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+                                    <br />      
+                                  <div id='ErrorMsgDiv'>
+                                  <span id='Message'></span>
+                                  </div>
                             
                                     <button type="submit" className="forgot_submit">Submit</button>
                                     <br />            
@@ -46,7 +93,7 @@ export default function ForgotPassword(){
 
                     </div>
                 </section>
-            <Footer/>
+           
         </div>
     )
 }
--- frontend/src/pages/Reset.js
+++ frontend/src/pages/Reset.js
@@ -3,10 +3,57 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import "../components/reset.css";
 import Passowrdresetimg01 from "../assests/Passwordreset img 01.png";
+import { useState } from 'react'
+import axios from 'axios';
 
 export default function Reset(){
+    const [password1,setPassword1]=useState(' ');
+    const [password2,setPassword2]=useState(' ');
+
+    const queryParameters = new URLSearchParams(window.location.search)
+    const email = queryParameters.get("email")
+    const otp = queryParameters.get("otp")
+
+    
+
+    const postData = {
+      email,
+      otp
+    };
+    
+    axios.post(
+      'http://localhost:8080/ecommerce/changepassword',
+      postData,
+      ).then(response=>{
+        console.log(response.data)
+        if (response.data!=='Create new password') {
+          document.getElementById('ChangePassword').innerHTML="<br/>"+response.data+"<br/>";
+        }
+      }
+       )
+
+
+
+function ChangePassword(params) {
+const passwordResetDto={
+    email,
+    password1
+};
+    
+    axios.post(
+        'http://localhost:8080/ecommerce/addnewpassword',
+        passwordResetDto,
+        ).then(response=>{
+
+
+        }
+        )
+
+    
+}
+
     return(
-        <div>
+        <div id='ChangePassword'>
             <Header />
 
             <section className="reset-password-section">
@@ -19,13 +66,13 @@ export default function Reset(){
                            
 
                             
-                                <form >                                  
+                                <form onSubmit={ChangePassword}>                                  
                                     <label className='reset-form'>Password</label><br/>
-                                    <input type="password" placeholder="Password" className='reset-form01'></input>
+                                    <input type="password" placeholder="Password" className='reset-form01'  onChange={(e)=>setPassword1(e.target.value)} ></input>
                                     <br /> 
                                   
                                     <label className="reset-form">Confirm Password</label><br/>
-                                    <input type="password" placeholder=" Confirm Password" className='reset-form01'></input>
+                                    <input type="password" placeholder=" Confirm Password" className='reset-form01' onChange={(e)=>setPassword2(e.target.value)} ></input>
                                     <br />    
 
                                     <div className='reset-buttons'>
