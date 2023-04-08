ca53f1bbb21b820cc970d811a5fb942a91625e9b
Yadeeshani Shakilanka Edirisinghe <syadeeshani@gmail.com>
2023-04-08 08:56:56 +0530

    home page edited

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -55,14 +55,56 @@ export default function Home() {
       </div>
       </div>
 
-      <h3>Recommended Books</h3>
+      <h1 className='latest-name'>Latest Products</h1><hr />
+      <div className="dropdown">
+                 <button className="btn btn-secondary dropdown-toggle bg-transparent" 
+                  type="button" data-bs-toggle="dropdown" 
+                   aria-expanded="false">
+                    Categories
+                      </button>
+                      
+  <ul className="dropdown-menu">
+    <li><a className="dropdown-item" href="Store?query=Arts&Music">Arts & Music</a></li>
+    <li><a className="dropdown-item" href="Store?query=Biographs">Biographs</a></li>
+    <li><a className="dropdown-item" href="Store?query=Business">Business</a></li>
+    <li><a className="dropdown-item" href="Store?query=Comics">Comics</a></li>
+    <li><a className="dropdown-item" href="Store?query=Computer&Tech">Computer & Tech</a></li>
+    <li><a className="dropdown-item" href="Store?query=Cooking">Cooking</a></li>
+    <li><a className="dropdown-item" href="Store?query=Education">Education</a></li>
+    <li><a className="dropdown-item" href="Store?query=Entertainment">Entertainment</a></li>
+    <li><a className="dropdown-item" href="Store?query=Health&Fitness">Health & Fitness</a></li>
+    <li><a className="dropdown-item" href="Store?query=History">History</a></li>
+    <li><a className="dropdown-item" href="Store?query=Hobbies&Craft">Hobbies & Craft</a></li>
+    <li><a className="dropdown-item" href="Store?query=Home&Garden">Home & Garden</a></li>
+    <li><a className="dropdown-item" href="Store?query=Kids">Kids</a></li>
+    <li><a className="dropdown-item" href="Store?query=Medical">Medical</a></li>
+    <li><a className="dropdown-item" href="Store?query=Mysteries">Mysteries</a></li>
+    <li><a className="dropdown-item" href="Store?query=Parenting">Parenting</a></li>
+    <li><a className="dropdown-item" href="Store?query=Religion">Religion</a></li>
+    <li><a className="dropdown-item" href="Store?query=Romance">Romance</a></li>
+    <li><a className="dropdown-item" href="Store?query=Science&Math">Science & Math</a></li>
+    <li><a className="dropdown-item" href="Store?query=SocialScience">Social Science</a></li>
+    <li><a className="dropdown-item" href="Store?query=Sports">Sports</a></li>
+    <li><a className="dropdown-item" href="Store?query=Travel">Travel</a></li>
+    <li><a className="dropdown-item" href="Store?query=TrueCrime">True Crime</a></li>
+    <li><a className="dropdown-item" href="Store?query=Westerns">Westerns</a></li>
+  </ul>
+
+          </div>
+      
+      
       <div className='topcontain'>
-      <div className='items'>
-      <Link>
-      <img src={require("../Images/1.jpg")} className='imgs'/>
-         <h3>$3.00</h3>
-         <h3>Kite Runner</h3>
-      </Link> 
+      <div className='card'>
+      
+      <img src={require("../Images/1.jpg")} className='card-img-top'/>
+      <div class='card-body'>
+         <h5 class='card-title'>Kite runner</h5>
+         <p class='card-text'>$10.00</p>
+         <a href='#' className='btn btn-primary'>
+            Continue Shopping
+         </a>
+      </div>
+ 
       </div>
 
       <div className='items'>
